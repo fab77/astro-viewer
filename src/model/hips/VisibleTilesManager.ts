d62f86ca64e291e6dcbe900a5c89838c33396a3e
Fabrizio Giordano <fabrizio.giordano@ext.esa.int>
2026-09-15 15:54:58 +0200

    perf(hips): sample visible tiles in CSS coordinates

--- src/model/hips/VisibleTilesManager.ts
+++ src/model/hips/VisibleTilesManager.ts
@@ -138,8 +138,10 @@ export class VisibleTilesManager implements HealpixVisibilityState {
       }
     } else {
       const geomhealpix: Healpix = global.getHealpix(order);
-      const maxX = (webgl as GL).canvas.width;
-      const maxY = (webgl as GL).canvas.height;
+      const canvas = (webgl as GL).canvas as HTMLCanvasElement;
+      const canvasRect = canvas.getBoundingClientRect();
+      const maxX = canvasRect.width;
+      const maxY = canvasRect.height;
 
       // Sample a grid of screen points, project to the sphere, then to galactic
       for (let i = 0; i <= maxX; i += maxX / 30) {
